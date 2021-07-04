7e54212afe23a604bbddac750d3e956b39749deb
ehmusman <ehmusman@gmail.com>
2021-07-04 12:20:33 +0500

    JS piaic files are added

---
+++ javascript/18-for-loop/for-loop.js
@@ -0,0 +1,36 @@
+var table= parseFloat(prompt("write the number whose table you want"));
+var length= prompt("length of table should be ")
+length = parseInt(length)
+
+for (var i = 1 ; i<= length ; i++){
+    //if (i == 11){
+      //  continue;
+    //}
+    var t = table * i;
+    console.log(table+" X "+i+" = "+t);
+    //console.log("this")
+}
+
+/*var table= parseFloat(prompt("write the number whose table you want"));
+var length= prompt("length of table should be ")
+length = parseInt(length)
+
+for (var i = 1 ; i<= length ; i++){
+    if (i == 11){
+        break;
+    }
+    var t = table * i;
+    console.log(table+" X "+i+" = "+t);
+    //console.log("this")
+}
+
+
+var table= parseFloat(prompt("write the number whose table you want"));
+var length= prompt("length of table should be ")
+length = parseInt(length)
+
+for (var i = 1 ; i<= length ; i++){
+    var t = table * i;
+    console.log(table+" X "+i+" = "+t);
+    //console.log("this")
+}*/
